caa743786a0e112baff85373749f56a4a97fd014
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-10-09 19:46:52 +0100

    chore: bundle unenv into flat-ish files

--- packages/vite-plugin-cloudflare/tsup.config.ts
+++ packages/vite-plugin-cloudflare/tsup.config.ts
@@ -1,4 +1,14 @@
+import { env, nodeless, cloudflare } from 'unenv';
 import { defineConfig } from 'tsup';
+import { createRequire } from 'module';
+
+// We precompile the unenv modules into flat files so they can be
+// easily loaded into workerd
+const { alias } = env(nodeless, cloudflare);
+const require = createRequire(import.meta.url);
+const unenvModules = Array.from(new Set(Object.values(alias)))
+	.filter((entry) => entry.startsWith('unenv'))
+	.map((entry) => require.resolve(entry).replace(/\.cjs$/, '.mjs'));
 
 export default defineConfig([
 	{
@@ -17,5 +27,15 @@ export default defineConfig([
 		external: ['cloudflare:workers'],
 		noExternal: ['vite/module-runner'],
 		tsconfig: 'tsconfig.runner.json',
+	},
+	{
+		entry: unenvModules,
+		outDir: 'dist/unenv/runtime',
+		platform: 'node',
+		format: 'esm',
+		splitting: false,
+		outExtension: () => ({ js: '.mjs' }),
+		noExternal: ['unenv'],
+		external: [/^node:/],
 	},
 ]);
