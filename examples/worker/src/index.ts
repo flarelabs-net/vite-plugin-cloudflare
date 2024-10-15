280a2a479ea90a43772a20df866cb67297780b66
Dario Piotrowicz <dario@cloudflare.com>
2024-10-15 17:36:03 +0100

    add the other third party module resolution examples from the integration test

--- examples/worker/src/index.ts
+++ examples/worker/src/index.ts
@@ -1,5 +1,13 @@
 import { OK } from 'zod';
 import React, { version as ReactVersion } from 'react';
+import { RPCErrorCodes, Utils } from 'discord-api-types/v10';
+import { Collection, SlashCreator, VERSION } from 'slash-create/web';
+
+const slashCreatorInstance = new SlashCreator({
+	applicationID: 'xxx',
+});
+
+const myCollection = new Collection([['a number', 54321]]);
 
 export default {
 	async fetch() {
@@ -7,6 +15,15 @@ export default {
 			'zod.OK.name': OK.name,
 			'typeof React': typeof React,
 			'react version': ReactVersion,
+			'(slash-create/web) VERSION': VERSION,
+			'(slash-create/web) slashCreatorInstance is instance of SlashCreator':
+				slashCreatorInstance instanceof SlashCreator,
+			'(slash-create/web) myCollection.random()': myCollection.random(),
+			'(discord-api-types/v10) Utils.isLinkButton({})': Utils.isLinkButton(
+				{} as any,
+			),
+			'(discord-api-types/v10) RPCErrorCodes.InvalidUser':
+				RPCErrorCodes.InvalidUser,
 		});
 	},
 };
