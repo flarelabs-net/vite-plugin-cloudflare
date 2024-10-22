b78c1aa1e9afec7509b40b542465dae162e709e3
Dario Piotrowicz <dario@cloudflare.com>
2024-10-22 12:17:57 +0100

    remove extra console.log

--- packages/vite-plugin-cloudflare/src/cloudflare-environment.ts
+++ packages/vite-plugin-cloudflare/src/cloudflare-environment.ts
@@ -120,10 +120,6 @@ export class CloudflareDevEnvironment extends vite.DevEnvironment {
 export function createCloudflareEnvironment(
 	options: WorkerOptions,
 ): vite.EnvironmentOptions {
-	console.log(
-		`\x1b[35m ===> ${JSON.stringify({ overrides: options.overrides })} \x1b[0m`,
-	);
-
 	return vite.mergeConfig(
 		{
 			resolve: {
