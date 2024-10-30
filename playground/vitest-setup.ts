8013a3c8b5d4c5eec56dea85fcf02c87822aebe0
Dario Piotrowicz <dario@cloudflare.com>
2024-10-30 12:10:35 +0000

    fix globalThis console type issue

--- playground/vitest-setup.ts
+++ playground/vitest-setup.ts
@@ -99,8 +99,7 @@ beforeAll(async (s) => {
 	browser = await chromium.connect(wsEndpoint);
 	page = await browser.newPage();
 
-	// @ts-ignore
-	const globalConsole = global.console;
+	const globalConsole = console;
 	const warn = globalConsole.warn;
 	globalConsole.warn = (msg: string, ...args: unknown[]) => {
 		if (msg.includes('Generated an empty chunk')) return;
