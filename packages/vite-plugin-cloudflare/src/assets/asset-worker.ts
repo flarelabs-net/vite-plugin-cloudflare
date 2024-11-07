f55a6399553b40cdf6632d9404feb684cab70b40
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2024-11-07 11:09:03 +0000

    Added build step for asset workers

---
+++ packages/vite-plugin-cloudflare/src/assets/asset-worker.ts
@@ -0,0 +1,4 @@
+// @ts-ignore
+import AssetWorker from '@cloudflare/workers-shared/dist/asset-worker.mjs';
+
+export default class CustomAssetWorker extends AssetWorker {}
--- playground/react-spa/vite.config.ts
+++ playground/react-spa/vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
 
 									res.end(html);
 								} catch (error) {
-									next();
+									next(error);
 								}
 							}
 						}
