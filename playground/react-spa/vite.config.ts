5e77ee8dd8eefe74778d575ded391d7edec7e3f0
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2024-11-07 10:18:10 +0000

    Test minimal implementation

--- playground/react-spa/vite.config.ts
+++ playground/react-spa/vite.config.ts
@@ -1,3 +1,6 @@
+import * as fs from 'node:fs';
+import * as fsp from 'node:fs/promises';
+import * as path from 'node:path';
 import { cloudflare } from '@flarelabs-net/vite-plugin-cloudflare';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
@@ -5,6 +8,39 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [
 		react(),
+		{
+			name: 'test-plugin',
+			configureServer(viteDevServer) {
+				const root = viteDevServer.config.root;
+
+				return () => {
+					viteDevServer.middlewares.use(async (req, res, next) => {
+						console.log(req.url, req.originalUrl);
+						// Clean url?
+						const url = req.url;
+
+						if (
+							url?.endsWith('.html') &&
+							req.headers['sec-fetch-dest'] !== 'script'
+						) {
+							const filePath = path.join(root, decodeURIComponent(url));
+
+							if (fs.existsSync(filePath)) {
+								// Add headers?
+								try {
+									let html = await fsp.readFile(filePath, 'utf-8');
+									html = await viteDevServer.transformIndexHtml(url, html);
+
+									res.end(html);
+								} catch (error) {
+									next();
+								}
+							}
+						}
+					});
+				};
+			},
+		},
 		// cloudflare({
 		// 	workers: {
 		// 		worker: {
