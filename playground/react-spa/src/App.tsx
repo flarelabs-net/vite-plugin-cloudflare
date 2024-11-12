30dba6a663d30f4284bb80a2a2e56a9e235db11d
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2024-11-12 12:30:46 +0000

    Added tests for react-spa

--- playground/react-spa/src/App.tsx
+++ playground/react-spa/src/App.tsx
@@ -18,7 +18,10 @@ function App() {
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
+				<button
+					onClick={() => setCount((count) => count + 1)}
+					aria-label="increment"
+				>
 					count is {count}
 				</button>
 				<p>
