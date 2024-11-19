bfe0e5d51116d55c7ae9fd217638468bcc3d0fd7
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2024-11-19 17:13:18 +0000

    Updated comments

--- playground/node-compat/__tests__/worker-basic/basic.spec.ts
+++ playground/node-compat/__tests__/worker-basic/basic.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest';
 import { getTextResponse, isBuild } from '../../../__test-utils__';
 
-// TODO: add build test
+// TODO: test build
 test.runIf(!isBuild)('basic nodejs properties', async () => {
 	const result = await getTextResponse();
 	expect(result).toBe(`"OK!"`);
