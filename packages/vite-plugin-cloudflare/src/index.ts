21be809daeb5799cb2076bf83ac10cd7b56827f8
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2024-12-02 12:11:06 +0000

    Added new resolvePluginConfig function

--- packages/vite-plugin-cloudflare/src/index.ts
+++ packages/vite-plugin-cloudflare/src/index.ts
@@ -17,245 +17,246 @@ import {
 	injectGlobalCode,
 	resolveNodeAliases,
 } from './node-js-compat';
-import { normalizePluginConfig } from './plugin-config';
+import { resolvePluginConfig } from './plugin-config';
 import { invariant } from './shared';
 import { toMiniflareRequest } from './utils';
-import type {
-	NormalizedPluginConfig,
-	PluginConfig,
-	WorkerOptions,
-} from './plugin-config';
-import type { RawConfig } from 'wrangler';
+import type { PluginConfig } from './plugin-config';
 
 // This is temporary
 let hasClientEnvironment = false;
 
-export function cloudflare<T extends Record<string, WorkerOptions>>(
-	pluginConfig: PluginConfig<T>,
-): vite.Plugin {
-	let mode: string | undefined;
-	let viteConfig: vite.ResolvedConfig;
-	let normalizedPluginConfig: NormalizedPluginConfig;
+export function cloudflare(pluginConfig: PluginConfig = {}): vite.Plugin {
+	// let mode: string | undefined;
+	// let viteConfig: vite.ResolvedConfig;
+	// let normalizedPluginConfig: NormalizedPluginConfig;
 
 	return {
 		name: 'vite-plugin-cloudflare',
 		config(userConfig) {
-			// We use the mode from the user config rather than the resolved config for now so that the mode has to be set explicitly
-			// Passing an `env` value to `readConfig` will lead to unexpected behaviour if the given environment does not exist in the user's config
-			mode = userConfig.mode;
-
-			return {
-				resolve: {
-					alias: getNodeCompatAliases(),
-				},
-				appType: 'custom',
-				builder: {
-					async buildApp(builder) {
-						const client = builder.environments.client;
-						const defaultHtmlPath = path.resolve(
-							builder.config.root,
-							'index.html',
-						);
-
-						if (
-							client &&
-							(client.config.build.rollupOptions.input ||
-								fs.existsSync(defaultHtmlPath))
-						) {
-							hasClientEnvironment = true;
-							await builder.build(client);
-						}
-
-						const workerEnvironments = Object.keys(
-							pluginConfig.workers ?? {},
-						).map((name) => {
-							const environment = builder.environments[name];
-							invariant(environment, `${name} environment not found`);
-
-							return environment;
-						});
-
-						await Promise.all(
-							workerEnvironments.map((environment) =>
-								builder.build(environment),
-							),
-						);
-					},
-				},
-				// Ensure there is an environment for each worker
-				environments: Object.fromEntries(
-					Object.entries(pluginConfig.workers ?? {}).map(
-						([name, workerOptions]) => [
-							name,
-							createCloudflareEnvironmentOptions(workerOptions, userConfig),
-						],
-					),
-				),
-			};
-		},
-		configEnvironment(name, options) {
-			options.build = {
-				...options.build,
-				// Puts all environment builds in subdirectories of the same build directory
-				outDir: path.join(options.build?.outDir ?? 'dist', name),
-			};
-		},
-		configResolved(resolvedConfig) {
-			viteConfig = resolvedConfig;
-			normalizedPluginConfig = normalizePluginConfig(
+			const resolvedPluginConfig = resolvePluginConfig(
 				pluginConfig,
-				resolvedConfig,
-				mode,
-			);
-		},
-		generateBundle() {
-			let config: RawConfig;
-
-			if (
-				this.environment.name === 'client' &&
-				!Object.keys(normalizedPluginConfig.workers).length
-			) {
-				config = {
-					assets: {
-						...normalizedPluginConfig.assets,
-						directory: '.',
-					},
-				};
-			} else {
-				const worker = normalizedPluginConfig.workers[this.environment.name];
-
-				if (!worker) {
-					return;
-				}
-
-				config = worker.wranglerConfig;
-
-				const isEntryWorker =
-					this.environment.name === normalizedPluginConfig.entryWorkerName;
-
-				if (isEntryWorker && hasClientEnvironment) {
-					config.assets = {
-						...normalizedPluginConfig.assets,
-						directory: path.join('..', 'client'),
-						binding:
-							normalizedPluginConfig.workers[this.environment.name]
-								?.assetsBinding,
-					};
-				}
-			}
-
-			this.emitFile({
-				type: 'asset',
-				fileName: 'wrangler.json',
-				source: JSON.stringify(config),
-			});
-		},
-		async resolveId(source) {
-			const worker = normalizedPluginConfig.workers[this.environment.name];
-			if (worker) {
-				const aliased = resolveNodeAliases(source, worker.workerOptions);
-				if (aliased) {
-					if (aliased.external) {
-						return aliased.id;
-					} else {
-						return await this.resolve(aliased.id);
-					}
-				}
-			}
-		},
-		async transform(code, id) {
-			const worker = normalizedPluginConfig.workers[this.environment.name];
-			if (worker) {
-				const rId = await this.resolve(worker.entryPath);
-				if (id === rId?.id) {
-					return injectGlobalCode(id, code, worker.workerOptions);
-				}
-			}
-		},
-		async configureServer(viteDevServer) {
-			let error: unknown;
-
-			const miniflare = new Miniflare(
-				getDevMiniflareOptions(
-					normalizedPluginConfig,
-					viteConfig,
-					viteDevServer,
-				),
+				userConfig,
 			);
 
-			await initRunners(normalizedPluginConfig, miniflare, viteDevServer);
-
-			viteDevServer.watcher.on('all', async (_, path) => {
-				if (!normalizedPluginConfig.wranglerConfigPaths.has(path)) {
-					return;
-				}
-
-				try {
-					await miniflare.setOptions(
-						getDevMiniflareOptions(
-							normalizedPluginConfig,
-							viteConfig,
-							viteDevServer,
-						),
-					);
-
-					await initRunners(normalizedPluginConfig, miniflare, viteDevServer);
-
-					error = undefined;
-					viteDevServer.environments.client.hot.send({ type: 'full-reload' });
-				} catch (err) {
-					error = err;
-					viteDevServer.environments.client.hot.send({ type: 'full-reload' });
-				}
-			});
-
-			const middleware = createMiddleware(async ({ request }) => {
-				const routerWorker = await getRouterWorker(miniflare);
-
-				return routerWorker.fetch(toMiniflareRequest(request), {
-					redirect: 'manual',
-				}) as any;
-			});
-
-			return () => {
-				viteDevServer.middlewares.use((req, res, next) => {
-					if (error) {
-						throw error;
-					}
-
-					if (!middleware) {
-						next();
-						return;
-					}
-
-					middleware(req, res, next);
-				});
-			};
+			console.log(resolvedPluginConfig);
 		},
-		configurePreviewServer(vitePreviewServer) {
-			const miniflare = new Miniflare(
-				getPreviewMiniflareOptions(normalizedPluginConfig, viteConfig),
-			);
+		// config(userConfig) {
+		// 	// We use the mode from the user config rather than the resolved config for now so that the mode has to be set explicitly
+		// 	// Passing an `env` value to `readConfig` will lead to unexpected behaviour if the given environment does not exist in the user's config
+		// 	// mode = userConfig.mode;
 
-			const middleware = createMiddleware(
-				({ request }) => {
-					return miniflare.dispatchFetch(toMiniflareRequest(request), {
-						redirect: 'manual',
-					}) as any;
-				},
-				{ alwaysCallNext: false },
-			);
+		// 	return {
+		// 		resolve: {
+		// 			alias: getNodeCompatAliases(),
+		// 		},
+		// 		appType: 'custom',
+		// 		builder: {
+		// 			async buildApp(builder) {
+		// 				const client = builder.environments.client;
+		// 				const defaultHtmlPath = path.resolve(
+		// 					builder.config.root,
+		// 					'index.html',
+		// 				);
 
-			return () => {
-				vitePreviewServer.middlewares.use((req, res, next) => {
-					if (!middleware) {
-						next();
-						return;
-					}
+		// 				if (
+		// 					client &&
+		// 					(client.config.build.rollupOptions.input ||
+		// 						fs.existsSync(defaultHtmlPath))
+		// 				) {
+		// 					hasClientEnvironment = true;
+		// 					await builder.build(client);
+		// 				}
 
-					middleware(req, res, next);
-				});
-			};
-		},
+		// 				const workerEnvironments = Object.keys(
+		// 					pluginConfig.workers ?? {},
+		// 				).map((name) => {
+		// 					const environment = builder.environments[name];
+		// 					invariant(environment, `${name} environment not found`);
+
+		// 					return environment;
+		// 				});
+
+		// 				await Promise.all(
+		// 					workerEnvironments.map((environment) =>
+		// 						builder.build(environment),
+		// 					),
+		// 				);
+		// 			},
+		// 		},
+		// 		// Ensure there is an environment for each worker
+		// 		environments: Object.fromEntries(
+		// 			Object.entries(pluginConfig.workers ?? {}).map(
+		// 				([name, workerOptions]) => [
+		// 					name,
+		// 					createCloudflareEnvironmentOptions(workerOptions, userConfig),
+		// 				],
+		// 			),
+		// 		),
+		// 	};
+		// },
+		// configEnvironment(name, options) {
+		// 	options.build = {
+		// 		...options.build,
+		// 		// Puts all environment builds in subdirectories of the same build directory
+		// 		outDir: path.join(options.build?.outDir ?? 'dist', name),
+		// 	};
+		// },
+		// configResolved(resolvedConfig) {
+		// 	viteConfig = resolvedConfig;
+		// 	normalizedPluginConfig = normalizePluginConfig(
+		// 		pluginConfig,
+		// 		resolvedConfig,
+		// 		mode,
+		// 	);
+		// },
+		// generateBundle() {
+		// 	let config: RawConfig;
+
+		// 	if (
+		// 		this.environment.name === 'client' &&
+		// 		!Object.keys(normalizedPluginConfig.workers).length
+		// 	) {
+		// 		config = {
+		// 			assets: {
+		// 				...normalizedPluginConfig.assets,
+		// 				directory: '.',
+		// 			},
+		// 		};
+		// 	} else {
+		// 		const worker = normalizedPluginConfig.workers[this.environment.name];
+
+		// 		if (!worker) {
+		// 			return;
+		// 		}
+
+		// 		config = worker.wranglerConfig;
+
+		// 		const isEntryWorker =
+		// 			this.environment.name === normalizedPluginConfig.entryWorkerName;
+
+		// 		if (isEntryWorker && hasClientEnvironment) {
+		// 			config.assets = {
+		// 				...normalizedPluginConfig.assets,
+		// 				directory: path.join('..', 'client'),
+		// 				binding:
+		// 					normalizedPluginConfig.workers[this.environment.name]
+		// 						?.assetsBinding,
+		// 			};
+		// 		}
+		// 	}
+
+		// 	this.emitFile({
+		// 		type: 'asset',
+		// 		fileName: 'wrangler.json',
+		// 		source: JSON.stringify(config),
+		// 	});
+		// },
+		// async resolveId(source) {
+		// 	const worker = normalizedPluginConfig.workers[this.environment.name];
+		// 	if (worker) {
+		// 		const aliased = resolveNodeAliases(source, worker.workerOptions);
+		// 		if (aliased) {
+		// 			if (aliased.external) {
+		// 				return aliased.id;
+		// 			} else {
+		// 				return await this.resolve(aliased.id);
+		// 			}
+		// 		}
+		// 	}
+		// },
+		// async transform(code, id) {
+		// 	const worker = normalizedPluginConfig.workers[this.environment.name];
+		// 	if (worker) {
+		// 		const rId = await this.resolve(worker.entryPath);
+		// 		if (id === rId?.id) {
+		// 			return injectGlobalCode(id, code, worker.workerOptions);
+		// 		}
+		// 	}
+		// },
+		// async configureServer(viteDevServer) {
+		// 	let error: unknown;
+
+		// 	const miniflare = new Miniflare(
+		// 		getDevMiniflareOptions(
+		// 			normalizedPluginConfig,
+		// 			viteConfig,
+		// 			viteDevServer,
+		// 		),
+		// 	);
+
+		// 	await initRunners(normalizedPluginConfig, miniflare, viteDevServer);
+
+		// 	viteDevServer.watcher.on('all', async (_, path) => {
+		// 		if (!normalizedPluginConfig.wranglerConfigPaths.has(path)) {
+		// 			return;
+		// 		}
+
+		// 		try {
+		// 			await miniflare.setOptions(
+		// 				getDevMiniflareOptions(
+		// 					normalizedPluginConfig,
+		// 					viteConfig,
+		// 					viteDevServer,
+		// 				),
+		// 			);
+
+		// 			await initRunners(normalizedPluginConfig, miniflare, viteDevServer);
+
+		// 			error = undefined;
+		// 			viteDevServer.environments.client.hot.send({ type: 'full-reload' });
+		// 		} catch (err) {
+		// 			error = err;
+		// 			viteDevServer.environments.client.hot.send({ type: 'full-reload' });
+		// 		}
+		// 	});
+
+		// 	const middleware = createMiddleware(async ({ request }) => {
+		// 		const routerWorker = await getRouterWorker(miniflare);
+
+		// 		return routerWorker.fetch(toMiniflareRequest(request), {
+		// 			redirect: 'manual',
+		// 		}) as any;
+		// 	});
+
+		// 	return () => {
+		// 		viteDevServer.middlewares.use((req, res, next) => {
+		// 			if (error) {
+		// 				throw error;
+		// 			}
+
+		// 			if (!middleware) {
+		// 				next();
+		// 				return;
+		// 			}
+
+		// 			middleware(req, res, next);
+		// 		});
+		// 	};
+		// },
+		// configurePreviewServer(vitePreviewServer) {
+		// 	const miniflare = new Miniflare(
+		// 		getPreviewMiniflareOptions(normalizedPluginConfig, viteConfig),
+		// 	);
+
+		// 	const middleware = createMiddleware(
+		// 		({ request }) => {
+		// 			return miniflare.dispatchFetch(toMiniflareRequest(request), {
+		// 				redirect: 'manual',
+		// 			}) as any;
+		// 		},
+		// 		{ alwaysCallNext: false },
+		// 	);
+
+		// 	return () => {
+		// 		vitePreviewServer.middlewares.use((req, res, next) => {
+		// 			if (!middleware) {
+		// 				next();
+		// 				return;
+		// 			}
+
+		// 			middleware(req, res, next);
+		// 		});
+		// 	};
+		// },
 	};
 }
--- packages/vite-plugin-cloudflare/src/plugin-config.ts
+++ packages/vite-plugin-cloudflare/src/plugin-config.ts
@@ -1,112 +1,213 @@
+import * as fs from 'node:fs';
 import * as path from 'node:path';
 import * as vite from 'vite';
 import { readConfig, unstable_getMiniflareWorkerOptions } from 'wrangler';
+import { invariant } from './shared';
 import type { AssetConfig } from './assets';
-import type { Config, SourcelessWorkerOptions } from 'wrangler';
+import type {
+	SourcelessWorkerOptions,
+	Config as WranglerConfig,
+} from 'wrangler';
 
-export interface WorkerOptions {
-	main: string;
+export interface PluginConfig {
 	wranglerConfig?: string;
-	// TODO: tighten up types so assets can only be bound to entry worker
-	assetsBinding?: string;
-	overrides?: vite.EnvironmentOptions;
+	viteEnvironment?: string;
+	auxiliaryWorkers?: Array<{
+		wranglerConfig: string;
+		viteEnvironment?: string;
+	}>;
 }
 
-export interface PluginConfig<
-	TWorkers extends Record<string, WorkerOptions> = Record<
-		string,
-		WorkerOptions
-	>,
-	TEntryWorker extends string = Extract<keyof TWorkers, string>,
-> {
-	workers?: TWorkers;
-	entryWorker?: TEntryWorker;
-	assets?: AssetConfig;
-	persistTo?: string | false;
-}
-
-export interface NormalizedPluginConfig {
-	workers: Record<
-		string,
-		{
-			entryPath: string;
-			wranglerConfigPath: string;
-			wranglerConfig: Config;
-			assetsBinding?: string;
-			workerOptions: SourcelessWorkerOptions & { name: string };
-		}
-	>;
-	entryWorkerName?: string;
-	assets: AssetConfig;
-	persistPath: string | false;
+export interface ResolvedPluginConfig {
+	workers: Record<string, WranglerConfig & { name: string }>;
+	entryWorkerName: string;
 	wranglerConfigPaths: Set<string>;
 }
 
-const DEFAULT_PERSIST_PATH = '.wrangler/state/v3';
+function getWorkerConfig(
+	userConfig: vite.UserConfig,
+	wranglerConfigPaths: Set<string>,
+	isEntryWorker: boolean,
+	configPath?: string,
+): WranglerConfig & { name: string } {
+	const root = userConfig.root ? path.resolve(userConfig.root) : process.cwd();
+	const resolvedConfigPath = configPath && path.join(root, configPath);
+	const wranglerConfig = readConfig(resolvedConfigPath, {
+		// We use the mode from the user config rather than the resolved config for now so that the mode has to be set explicitly
+		env: userConfig.mode,
+	});
+	invariant(wranglerConfig.configPath, `Unexpected error: No config path`);
 
-export function normalizePluginConfig(
-	pluginConfig: PluginConfig,
-	viteConfig: vite.ResolvedConfig,
-	mode?: string,
-): NormalizedPluginConfig {
-	const wranglerConfigPaths = new Set<string>();
-	const workers = Object.fromEntries(
-		Object.entries(pluginConfig.workers ?? {}).map(([name, options]) => {
-			const wranglerConfigPath = path.resolve(
-				viteConfig.root,
-				options.wranglerConfig ?? './wrangler.toml',
-			);
+	if (wranglerConfigPaths.has(wranglerConfig.configPath)) {
+		throw new Error(
+			`Duplicate Wrangler config path found: ${wranglerConfig.configPath}`,
+		);
+	}
 
-			if (wranglerConfigPaths.has(wranglerConfigPath)) {
-				throw new Error(
-					`Duplicate Wrangler config path found: ${wranglerConfigPath}`,
-				);
-			}
+	wranglerConfigPaths.add(wranglerConfig.configPath);
 
-			const wranglerConfig = readConfig(wranglerConfigPath, {
-				env: mode,
-			});
+	if (isEntryWorker && !wranglerConfig.main) {
+		invariant(
+			wranglerConfig.assets,
+			`No main or assets field provided in ${wranglerConfig.configPath}`,
+		);
 
-			wranglerConfigPaths.add(wranglerConfigPath);
+		const workerName = wranglerConfig.name ?? 'assets-only';
 
-			// We'll need to change this to `${name}-${mode}` when the `mode` is present but this is non-trivial because service bindings etc. will need updating
-			wranglerConfig.name = name;
+		return { ...wranglerConfig, name: workerName };
+	}
 
-			const miniflareWorkerOptions =
-				unstable_getMiniflareWorkerOptions(wranglerConfig);
-
-			const { ratelimits, ...workerOptions } =
-				miniflareWorkerOptions.workerOptions;
-
-			return [
-				name,
-				{
-					assetsBinding: options.assetsBinding,
-					entryPath: options.main,
-					wranglerConfigPath,
-					wranglerConfig,
-					// `unstable_getMiniflareWorkerOptions` always sets the `name` to undefined so we have to add it again here
-					workerOptions: { ...workerOptions, name },
-				},
-			];
-		}),
+	invariant(
+		wranglerConfig.main,
+		`No main field provided in ${wranglerConfig.configPath}`,
 	);
 
-	const assets = pluginConfig.assets ?? {};
+	invariant(
+		wranglerConfig.name,
+		`No name field provided in ${wranglerConfig.configPath}`,
+	);
 
-	const persistPath =
-		pluginConfig.persistTo === false
-			? false
-			: path.resolve(
-					viteConfig.root,
-					pluginConfig.persistTo ?? DEFAULT_PERSIST_PATH,
-				);
+	return { ...wranglerConfig, name: wranglerConfig.name };
+}
+
+export function resolvePluginConfig(
+	pluginConfig: PluginConfig,
+	userConfig: vite.UserConfig,
+): ResolvedPluginConfig {
+	const wranglerConfigPaths = new Set<string>();
+
+	const entryWorkerConfig = getWorkerConfig(
+		userConfig,
+		wranglerConfigPaths,
+		true,
+		pluginConfig.wranglerConfig,
+	);
+
+	const workers = {
+		[entryWorkerConfig.name]: entryWorkerConfig,
+	};
+
+	for (const auxiliaryWorker of pluginConfig.auxiliaryWorkers ?? []) {
+		const workerConfig = getWorkerConfig(
+			userConfig,
+			wranglerConfigPaths,
+			false,
+			auxiliaryWorker.wranglerConfig,
+		);
+
+		workers[workerConfig.name] = workerConfig;
+	}
 
 	return {
-		workers,
-		entryWorkerName: pluginConfig.entryWorker,
-		assets,
-		persistPath,
 		wranglerConfigPaths,
+		workers,
+		entryWorkerName: entryWorkerConfig.name,
 	};
 }
+
+// export interface WorkerOptions {
+// 	main: string;
+// 	wranglerConfig?: string;
+// 	// TODO: tighten up types so assets can only be bound to entry worker
+// 	assetsBinding?: string;
+// 	overrides?: vite.EnvironmentOptions;
+// }
+
+// export interface PluginConfig<
+// 	TWorkers extends Record<string, WorkerOptions> = Record<
+// 		string,
+// 		WorkerOptions
+// 	>,
+// 	TEntryWorker extends string = Extract<keyof TWorkers, string>,
+// > {
+// 	workers?: TWorkers;
+// 	entryWorker?: TEntryWorker;
+// 	assets?: AssetConfig;
+// 	persistTo?: string | false;
+// }
+
+// export interface NormalizedPluginConfig {
+// 	workers: Record<
+// 		string,
+// 		{
+// 			entryPath: string;
+// 			wranglerConfigPath: string;
+// 			wranglerConfig: Config;
+// 			assetsBinding?: string;
+// 			workerOptions: SourcelessWorkerOptions & { name: string };
+// 		}
+// 	>;
+// 	entryWorkerName?: string;
+// 	assets: AssetConfig;
+// 	persistPath: string | false;
+// 	wranglerConfigPaths: Set<string>;
+// }
+
+// const DEFAULT_PERSIST_PATH = '.wrangler/state/v3';
+
+// export function normalizePluginConfig(
+// 	pluginConfig: PluginConfig,
+// 	viteConfig: vite.ResolvedConfig,
+// 	mode?: string,
+// ): NormalizedPluginConfig {
+// 	const wranglerConfigPaths = new Set<string>();
+// 	const workers = Object.fromEntries(
+// 		Object.entries(pluginConfig.workers ?? {}).map(([name, options]) => {
+// 			const wranglerConfigPath = path.resolve(
+// 				viteConfig.root,
+// 				options.wranglerConfig ?? './wrangler.toml',
+// 			);
+
+// 			if (wranglerConfigPaths.has(wranglerConfigPath)) {
+// 				throw new Error(
+// 					`Duplicate Wrangler config path found: ${wranglerConfigPath}`,
+// 				);
+// 			}
+
+// 			const wranglerConfig = readConfig(wranglerConfigPath, {
+// 				env: mode,
+// 			});
+
+// 			wranglerConfigPaths.add(wranglerConfigPath);
+
+// 			// We'll need to change this to `${name}-${mode}` when the `mode` is present but this is non-trivial because service bindings etc. will need updating
+// 			wranglerConfig.name = name;
+
+// 			const miniflareWorkerOptions =
+// 				unstable_getMiniflareWorkerOptions(wranglerConfig);
+
+// 			const { ratelimits, ...workerOptions } =
+// 				miniflareWorkerOptions.workerOptions;
+
+// 			return [
+// 				name,
+// 				{
+// 					assetsBinding: options.assetsBinding,
+// 					entryPath: options.main,
+// 					wranglerConfigPath,
+// 					wranglerConfig,
+// 					// `unstable_getMiniflareWorkerOptions` always sets the `name` to undefined so we have to add it again here
+// 					workerOptions: { ...workerOptions, name },
+// 				},
+// 			];
+// 		}),
+// 	);
+
+// 	const assets = pluginConfig.assets ?? {};
+
+// 	const persistPath =
+// 		pluginConfig.persistTo === false
+// 			? false
+// 			: path.resolve(
+// 					viteConfig.root,
+// 					pluginConfig.persistTo ?? DEFAULT_PERSIST_PATH,
+// 				);
+
+// 	return {
+// 		workers,
+// 		entryWorkerName: pluginConfig.entryWorker,
+// 		assets,
+// 		persistPath,
+// 		wranglerConfigPaths,
+// 	};
+// }
