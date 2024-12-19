d5aae065f6f7250e9924f32f7e007f45537aef42
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2024-12-19 11:00:35 +0000

    Refactored Workflows playground

--- playground/workflows/src/index.ts
+++ playground/workflows/src/index.ts
@@ -29,32 +29,30 @@ export default {
 		const id = url.searchParams.get('id');
 
 		if (url.pathname === '/create') {
-			let instance: WorkflowInstance;
-
-			if (id === null) {
-				instance = await env.MY_WORKFLOW.create();
-			} else {
-				instance = await env.MY_WORKFLOW.create({ id });
-			}
+			const instance = await env.MY_WORKFLOW.create(
+				id === null ? undefined : { id },
+			);
 
 			return Response.json({
 				id: instance.id,
 				status: await instance.status(),
 			});
-		} else if (url.pathname === '/get') {
+		}
+
+		if (url.pathname === '/get') {
 			if (id === null) {
 				return new Response(
 					'Please provide an id (`/get?id=unique-instance-id`)',
 				);
-			} else {
-				const instance = await env.MY_WORKFLOW.get(id);
-
-				return Response.json(await instance.status());
 			}
-		} else {
-			return new Response(
-				'Create a new Workflow instance (`/create` or `/create?id=unique-instance-id`) or inspect an existing instance (`/get?id=unique-instance-id`).',
-			);
+
+			const instance = await env.MY_WORKFLOW.get(id);
+
+			return Response.json(await instance.status());
 		}
+
+		return new Response(
+			'Create a new Workflow instance (`/create` or `/create?id=unique-instance-id`) or inspect an existing instance (`/get?id=unique-instance-id`).',
+		);
 	},
 } satisfies ExportedHandler<Env>;
