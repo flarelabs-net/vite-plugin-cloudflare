7ec55923793372b63b64e49d14e113b46655eb5d
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2024-12-20 10:15:28 +0000

    Added unit tests for getOutputDirectory

---
+++ packages/vite-plugin-cloudflare/src/__tests__/utils.spec.ts
@@ -0,0 +1,34 @@
+import { describe, expect, test } from 'vitest';
+import { getOutputDirectory } from '../utils';
+
+describe('getOutputDirectory', () => {
+	test('returns the correct output if `environments[environmentName].build.outDir` is defined', () => {
+		expect(
+			getOutputDirectory(
+				{
+					environments: {
+						worker: {
+							build: { outDir: 'custom-environment-output-directory' },
+						},
+					},
+				},
+				'worker',
+			),
+		).toBe('custom-environment-output-directory');
+	});
+
+	test('returns the correct output if `environments[environmentName].build.outDir` is not defined and `build.outDir` is defined', () => {
+		expect(
+			getOutputDirectory(
+				{ build: { outDir: 'custom-root-output-directory' } },
+				'environment-name',
+			),
+		).toBe('custom-root-output-directory/environment-name');
+	});
+
+	test('returns the correct output if `environments[environmentName].build.outDir` and `build.outDir` are not defined', () => {
+		expect(getOutputDirectory({}, 'environment-name')).toBe(
+			'dist/environment-name',
+		);
+	});
+});
