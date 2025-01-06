4f1d443a8724b6fc5a0e86bef97460b64bc7a8b4
Pete Bacon Darwin <pete@bacondarwin.com>
2025-01-06 10:05:49 +0000

    Re-enable Node.js compatibility (#123)
    It turned out that the problems needing us to hack the `/@id/...` imports were arising because we were also creating virtual modules for Node.js aliases that were already actually expected to be external.
    
    Fixes https://github.com/flarelabs-net/vite-plugin-cloudflare/issues/63

--- playground/node-compat/__tests__/worker-random/random.spec.ts
+++ playground/node-compat/__tests__/worker-random/random.spec.ts
@@ -1,8 +1,7 @@
 import { expect, test } from 'vitest';
 import { getJsonResponse } from '../../../__test-utils__';
 
-// TODO: reintroduce test in #63
-test.skip('should be able to call `getRandomValues()` bound to any object', async () => {
+test('should be able to call `getRandomValues()` bound to any object', async () => {
 	const result = await getJsonResponse();
 	expect(result).toEqual([
 		expect.any(String),
