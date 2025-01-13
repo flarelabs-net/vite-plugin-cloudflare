94d1c8e67071877c16d51dc0572ab576d349a13e
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-01-13 13:06:23 +0000

    Added websockets playground

---
+++ playground/websockets/src/index.ts
@@ -0,0 +1,50 @@
+import { DurableObject } from 'cloudflare:workers';
+
+interface Env {
+	WEBSOCKET_SERVER: DurableObjectNamespace<WebSocketServer>;
+}
+
+export class WebSocketServer extends DurableObject {
+	#currentlyConnectedWebSockets = 0;
+
+	override async fetch(request: Request) {
+		const { 0: client, 1: server } = new WebSocketPair();
+
+		server.accept();
+		this.#currentlyConnectedWebSockets += 1;
+
+		server.addEventListener('message', (event) => {
+			server.send(
+				`[Durable Object] currentlyConnectedWebSockets: ${this.#currentlyConnectedWebSockets}`,
+			);
+		});
+
+		server.addEventListener('close', (event) => {
+			this.#currentlyConnectedWebSockets -= 1;
+			server.close(event.code, 'Durable Object is closing WebSocket');
+		});
+
+		return new Response(null, { status: 101, webSocket: client });
+	}
+}
+
+export default {
+	async fetch(request, env) {
+		if (request.url.endsWith('/websocket')) {
+			const upgradeHeader = request.headers.get('Upgrade');
+
+			if (!upgradeHeader || upgradeHeader !== 'websocket') {
+				return new Response('Durable Object expected Upgrade: websocket', {
+					status: 426,
+				});
+			}
+
+			const id = env.WEBSOCKET_SERVER.idFromName('');
+			const stub = env.WEBSOCKET_SERVER.get(id);
+
+			return stub.fetch(request);
+		}
+
+		return new Response(null, { status: 400 });
+	},
+} satisfies ExportedHandler<Env>;
