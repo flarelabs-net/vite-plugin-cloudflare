d2c1e018c3b85aa9ba7e0963ab970afd81c581c6
Dario Piotrowicz <dario@cloudflare.com>
2025-01-13 15:48:33 +0000

    remove windows specific snapshot

--- packages/vite-plugin-cloudflare/src/__tests__/get-warning-for-workers-resolved-configs.spec.ts
+++ packages/vite-plugin-cloudflare/src/__tests__/get-warning-for-workers-resolved-configs.spec.ts
@@ -148,21 +148,12 @@ describe('getWarningForWorkersConfigs', () => {
 				},
 			],
 		});
-		if (process.platform === 'win32') {
-			expect(warning).toMatchInlineSnapshot(`
-				"
-				[43mWARNING[0m: your workers configs contain configuration options which are ignored since they are not applicable when using Vite:
-				  - (entry) worker "entry-worker" (config at \`wrangler.json\`)
-				    - \`alias\` which is replaced by Vite's \`resolve.alias\` (docs: https://vite.dev/config/shared-options.html#resolve-alias)
-				    - \`build\` which is not relevant in the context of a Vite project
-				  - (auxiliary) worker "worker-a" (config at \`a\\wrangler.json\`)
-				    - \`find_additional_modules\`, \`no_bundle\` which are not relevant in the context of a Vite project
-				  - (auxiliary) worker (config at \`b\\wrangler.json\`)
-				    - \`site\` which is not relevant in the context of a Vite project
-				"
-			`);
-		} else {
-			expect(warning).toMatchInlineSnapshot(`
+		// Note: to make the snapshot work on windows we need to replace path backslashes into normal forward ones
+		const normalizedWarning = warning?.replaceAll(
+			'\\wrangler.json',
+			'/wrangler.json',
+		);
+		expect(normalizedWarning).toMatchInlineSnapshot(`
 				"
 				[43mWARNING[0m: your workers configs contain configuration options which are ignored since they are not applicable when using Vite:
 				  - (entry) worker "entry-worker" (config at \`wrangler.json\`)
@@ -174,6 +165,5 @@ describe('getWarningForWorkersConfigs', () => {
 				    - \`site\` which is not relevant in the context of a Vite project
 				"
 			`);
-		}
 	});
 });
