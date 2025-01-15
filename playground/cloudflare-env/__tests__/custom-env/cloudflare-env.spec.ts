d2eada9b58d6bd234df7b6b67f7f8aabe04d860e
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-01-15 18:52:26 +0000

    Removed cloudflare-env custom-env test

--- playground/cloudflare-env/__tests__/custom-env/cloudflare-env.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from 'vitest';
-import { getTextResponse } from '../../../__test-utils__';
-
-test('returns the correct var when CLOUDFLARE_ENV is provided', async () => {
-	expect(await getTextResponse()).toEqual('Custom env var');
-});
